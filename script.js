3bd0b66a59c560b7623c31a0c852ee04671a62a7
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-07 14:31:52 +0100

    got result for intermediate exercise

--- script.js
+++ script.js
@@ -28,17 +28,57 @@
 
 // intermediate exercise 1
 
-let santaclaus = "shown";
+let santaclaus1 = "shown";
+let santaclaus2 = "shown";
+let santaclaus3 = "shown";
+let santaclaus4 = "shown";
 
-function hideSanta () {
-    if (santaclaus == "shown") {
-        document.getElementById(santa1).style.opacity = "0";
-        document.getElementById(santa1).style.transition = "all 1s";
-        santaclaus = "hidden";
+function hideSanta1 () {
+    if (santaclaus1 == "shown") {
+        document.getElementById("santa1").style.opacity = "0";
+        document.getElementById("santa1").style.transition = "all 1s";
+        santaclaus1 = "hidden";
     } else {
-        document.getElementById(santa1).style.opacity = "1";
-        santaclaus = "shown";
+        document.getElementById("santa1").style.opacity = "1";
+        santaclaus1 = "shown";
     }
 }
 
-document.getElementById(santa1).addEventListener("click", hideSanta);
+function hideSanta2 () {
+    if (santaclaus2 == "shown") {
+        document.getElementById("santa2").style.opacity = "0";
+        document.getElementById("santa2").style.transition = "all 1s";
+        santaclaus2 = "hidden";
+    } else {
+        document.getElementById("santa2").style.opacity = "1";
+        santaclaus2 = "shown";
+    }
+}
+function hideSanta3 () {
+    if (santaclaus3 == "shown") {
+        document.getElementById("santa3").style.opacity = "0";
+        document.getElementById("santa3").style.transition = "all 1s";
+        santaclaus3 = "hidden";
+    } else {
+        document.getElementById("santa3").style.opacity = "1";
+        santaclaus3 = "shown";
+    }
+}
+
+function hideSanta4 () {
+
+    if (santaclaus4 == "shown") {
+        document.getElementById("santa4").style.opacity = "0";
+        document.getElementById("santa4").style.transition = "all 1s";
+        santaclaus4 = "hidden";
+    } else {
+        document.getElementById("santa4").style.opacity = "1";
+        santaclaus4 = "shown";
+    }
+}
+
+document.getElementById("santa1").addEventListener("click", hideSanta1);
+document.getElementById("santa2").addEventListener("click", hideSanta2);
+document.getElementById("santa3").addEventListener("click", hideSanta3);
+document.getElementById("santa4").addEventListener("click", hideSanta4);
+
