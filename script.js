c4be057126f951d5a2e6791b13b72a30218d51f6
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-07 14:18:05 +0100

    struggling with intermediate

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-function submitInfo() {
-    let firstName = document.getElementById("fname").value;
-    let lastName = document.getElementById("lname").value;
-    let age = document.getElementById("age").value;
+// basic exercise
 
-    console.log(`My name is ${firstName} ${lastName}, I'm ${age} years old.`);
-    document.getElementById(divFirstName).innerHTML = `My name is ${firstName} ${lastName}, I'm ${age} years old.`;
+// function submitInfo() {
+//     let firstName = document.getElementById("fname").value;
+//     let lastName = document.getElementById("lname").value;
+//     let age = document.getElementById("age").value;
+//     let profession = document.getElementById("profession").value;
+
+//     console.log(`My name is ${firstName} ${lastName}, I'm ${age} years old.`);
+//     document.getElementById("result").innerHTML = `My name is <span id="color">${firstName}</span> ${lastName}, I'm ${age} years old. I work in ${profession}`;
+
+//     if (firstName.length >= 5) {
+//         document.getElementById("color").style.color = "green";
+//     } else {
+//         document.getElementById("color").style.color = "red";
+//     }
+
+//     if (profession == "IT") {
+//         document.getElementById("result").style.backgroundColor = "purple";
+//     } else {
+//         document.getElementById("result").style.backgroundColor = "yellow";
+//     }
     
+// }
+
+
+// document.getElementById("btn").addEventListener("click", submitInfo);
+
+// intermediate exercise 1
+
+let santaclaus = "shown";
+
+function hideSanta () {
+    if (santaclaus == "shown") {
+        document.getElementById(santa1).style.opacity = "0";
+        document.getElementById(santa1).style.transition = "all 1s";
+        santaclaus = "hidden";
+    } else {
+        document.getElementById(santa1).style.opacity = "1";
+        santaclaus = "shown";
+    }
 }
 
-
-document.getElementById("btn").addEventListener("click", submitInfo);
+document.getElementById(santa1).addEventListener("click", hideSanta);
